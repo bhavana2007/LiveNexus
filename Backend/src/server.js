7627d3a430cd05bb980cd930e8cbfe584c01c4f0
bhavana2007 <bhavanakolli983@gmail.com>
2026-02-15 19:59:31 +0530

    Update server configuration

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,17 +2,25 @@ import express from 'express';
 import dotenv from "dotenv";
 import messageRoutes from "./routes/message.route.js";
 import authRoutes from "./routes/auth.route.js";
-
+import path from "path";
 
 dotenv.config();
 
 const app=express();
+const __dirname=path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../Frontend/dist")));
+    app.get("*",(_,res)=>{
+        res.sendFile(path.join(__dirname,"../Frontend/dist/index.html"));
+    });
+}
 
 app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`));
 
